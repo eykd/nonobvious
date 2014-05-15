daf6d5d549ee4c589b569547bf9918b7dc5b53a3
deyk <deyk@crossway.org>
2014-05-15 13:43:56 -0700

    Ditched specloud. Let's do it with paver!

---
+++ pavement.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""pavement.py -- paver tasks for managing the nonobvious project.
+"""
+from paver.easy import task, sh
+
+
+@task
+def test():
+    """Run the test suite w/ nosetests, with coverage and pretty colors.
+    """
+    sh(" ".join([
+        "nosetests",
+        "-i",
+        "'^(it|ensure|must|should|deve|specs?|examples?)'",
+        "-i",
+        "'(specs?|examples?|exemplos?)(.py)?$'",
+        "--with-spec",
+        "--spec-color",
+        "--with-coverage",
+        "--cover-package=nonobvious",
+        "--nocapture",
+    ]))
+
+
+@task
+def dev():
+    """Set up the development environment.
+    """
+    sh("virtualenv .")
+    sh("bin/pip install -r dev-requirements.txt")
